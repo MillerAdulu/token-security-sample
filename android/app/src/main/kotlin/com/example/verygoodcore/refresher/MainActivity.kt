2ca8d839434e0d1c452b73097abb9295a71ef295
Miller Adulu <milleradulu@gmail.com>
2025-02-05 17:46:55 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/refresher/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.refresher
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:refresher/app/app.dart';
+import 'package:refresher/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
